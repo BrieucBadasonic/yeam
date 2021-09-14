4f2d637d84ffffece932eb2e7b74ba11db39c4ca
Brieuc Labiouse <brieuclabiouse@gmail.com>
2021-09-14 14:55:57 +0200

    generate report model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_124048) do
+ActiveRecord::Schema.define(version: 2021_09_14_124508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,19 @@ ActiveRecord::Schema.define(version: 2021_09_14_124048) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reports", force: :cascade do |t|
+    t.date "date"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "benevole_id", null: false
+    t.bigint "binome_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["benevole_id"], name: "index_reports_on_benevole_id"
+    t.index ["binome_id"], name: "index_reports_on_binome_id"
+    t.index ["user_id"], name: "index_reports_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,4 +80,7 @@ ActiveRecord::Schema.define(version: 2021_09_14_124048) do
   add_foreign_key "binomes", "benevoles"
   add_foreign_key "binomes", "clients"
   add_foreign_key "binomes", "users"
+  add_foreign_key "reports", "benevoles"
+  add_foreign_key "reports", "binomes"
+  add_foreign_key "reports", "users"
 end
