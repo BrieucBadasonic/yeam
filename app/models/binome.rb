4a67b45703a4ed608b7cef0ad5584f2e9a26d948
Brieuc Labiouse <brieuclabiouse@gmail.com>
2021-09-17 12:26:46 +0200

    search by request status

--- app/models/binome.rb
+++ app/models/binome.rb
@@ -8,4 +8,9 @@ class Binome < ApplicationRecord
   REQUEST_STATUS = ["RAS", "A SUIVRE", "CONTACT PERDU", "DOSSIER CLOTURE"]
 
   validates :request_status, inclusion: { in: REQUEST_STATUS }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_request_status,
+    against: [ :request_status ]
+
 end
