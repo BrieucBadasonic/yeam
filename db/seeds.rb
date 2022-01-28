76b8b48d316ff783b2bf688b04cab8197347b671
brieuc labiouse <brieuclabiouse@gmail.com>
2022-01-28 19:10:05 +0100

    concatenate full name from firest and last name

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,6 @@ user1 = User.new(password: "123456",
                  email: "brieuc@gmail.com",
                  first_name: "Brieuc",
                  last_name: "Labiouse",
-                 full_name: "Brieuc Labiouse",
                  telephone: "0479378074")
 user1.save
 
@@ -17,7 +16,6 @@ user2 = User.new(password: "belgacom11",
                  email: "olivier.beernaert@gmail.com",
                  first_name: "Olivier",
                  last_name: "Beernaert",
-                 full_name: "Olivier Beernaert",
                  telephone: "0489542334")
 user2.save
 
@@ -25,7 +23,6 @@ user3 = User.new(password: "123456",
                  email: "michel@gmail.com",
                  first_name: "Michel",
                  last_name: "Duchateau",
-                 full_name: "Michel Duchateau",
                  telephone: "0474672341")
 user3.save
 
@@ -33,20 +30,17 @@ user4 = User.new(password: "123456",
                  email: "Ines@gmail.com",
                  first_name: "Ines",
                  last_name: "Durban",
-                 full_name: "Ines Durban",
                  telephone: "0490756744")
 user4.save
 
 benevole1 = Benevole.new(first_name: "Adrien",
                          last_name: "Ducobu",
-                         full_name: "Adrien Ducobu",
                          telephone: "0473457612",
                          email: "Adrien@gmail.com")
 benevole1.save
 
 benevole2 = Benevole.new(first_name: "Chris",
                          last_name: "Laplante",
-                         full_name: "Chris Laplante",
                          telephone: "0448613853",
                          email: "Chris@gmail.com")
 benevole2.save
@@ -54,46 +48,40 @@ benevole2.save
 benevole3 = Benevole.new(first_name: "Thomas",
                          last_name: "Verrati",
                          telephone: "0473908944",
-                         full_name: "Thomas Verrati",
                          email: "Thomas@gmail.com")
 benevole3.save
 
 benevole4 = Benevole.new(first_name: "Lora",
                          last_name: "Friquet",
-                         full_name: "Lora Friquet",
                          telephone: "0497115632",
                          email: "Adrien@gmail.com")
 benevole4.save
 
-client1 = Client.new(first_name: "Client 1",
-                     last_name: "Client 1",
-                     full_name: "Client 1",
+client1 = Client.new(first_name: "Michel",
+                     last_name: "Gondry",
                      telephone: "0498784532",
-                     email: "Client1@gmail.com",
+                     email: "Michel@gmail.com",
                      residency: "Forest")
 client1.save
 
-client2 = Client.new(first_name: "client 2",
-                     last_name: "Client 2",
-                     full_name: "Client 2",
+client2 = Client.new(first_name: "Johan",
+                     last_name: "Mate",
                      telephone: "0498784532",
-                     email: "client2@gmail.com",
+                     email: "Johan@gmail.com",
                      residency: "Tournai")
 client2.save
 
-client3 = Client.new(first_name: "Client 3",
-                     last_name: "Client 3",
-                     full_name: "Client 3",
+client3 = Client.new(first_name: "Santi",
+                     last_name: "Logic",
                      telephone: "0498784532",
-                     email: "client3@gmail.com",
+                     email: "Santi@gmail.com",
                      residency: "Schaerbeek")
 client3.save
 
-client4 = Client.new(first_name: "Client 4",
-                     last_name: "Client 4",
-                     full_name: "Client 4",
+client4 = Client.new(first_name: "Adam",
+                     last_name: "Nagelne",
                      telephone: "0498784532",
-                     email: "client4@gmail.com",
+                     email: "Adam4@gmail.com",
                      residency: "Jette")
 client4.save
 
